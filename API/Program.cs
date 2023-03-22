98f0acba05683094fa48fc1a2a2da994b9505242
Matthew Kidd <mattofthekidd@users.noreply.github.com>
2023-03-22 14:27:22 -0600

    adds CORS policy for angular and setup the API request to display the users table on the site

--- API/Program.cs
+++ API/Program.cs
@@ -11,23 +11,16 @@ builder.Services.AddDbContext<DataContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); //Looks in the "appsettings" file
 });
 
+builder.Services.AddCors();
+
 //end of services container
 
 var app = builder.Build();
     
 //These are middleware
-// you can put things here like authorization
-    // // Configure the HTTP request pipeline.
-    // if (app.Environment.IsDevelopment())
-    // {
-    //     app.UseSwagger();
-    //     app.UseSwaggerUI();
-    // }
 
-    // app.UseHttpsRedirection();
-    // app.UseAuthorization();
-
-//This tells a request seeking an api end-point where to go
-app.MapControllers();
+// Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); //sets a CORS policy for our front-end end-point requests
+app.MapControllers(); //This tells a request seeking an api end-point where to go
 
 app.Run();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.http.get('https://localhost:5001/api/users').subscribe({
+      next: x => this.users = x,
+      error: error => console.log(error),
+      complete: () => console.log('Request completed'),
+    });
+  }
+
 }
