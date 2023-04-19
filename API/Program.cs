7963e4543203893a460ba6696ff93df9185102d3
Matthew Kidd <mattofthekidd@users.noreply.github.com>
2023-04-19 16:59:49 -0600

    end of sec 8.88 also kinda 89. Created seeder for the Users and Photos table

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 //main entry point into our application
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,8 +21,9 @@ var app = builder.Build();
 // }
 
 //These are middleware
-app.UseMiddleware<ExceptionMiddleware>();
+
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); //sets a CORS policy for our frontend endpoint requests
 
 //this must happen here and in this order, section 4.44
@@ -28,5 +31,21 @@ app.UseAuthentication(); //is the token valid?
 app.UseAuthorization(); //if token is valid, what does the token allow you to do?
 
 app.MapControllers(); //This tells a request seeking an api end-point where to go
+
+//This must occur after MapControllers
+//but before Run
+using var scope = app.Services.CreateScope(); //gives us access to all the services in Program.cs
+var services = scope.ServiceProvider;
+try {
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    //If we have an empty DB this will run the migrations
+    await Seed.SeedUsers(context);
+    // obviously running the user seeder
+}
+catch(Exception ex) {
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
